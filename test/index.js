2833dac1c565658234ba4e315c95337a810f14a2
Chris Zuber <shgysk8zer0@gmail.com>
2024-02-28 14:04:08 -0800

    Add `@aegisjsproject/*` & auto-update

--- test/index.js
+++ test/index.js
@@ -34,3 +34,17 @@ Promise.all([
 });
 
 konami().then(() => console.log('Cheat mode activated'));
+import { AegisComponent, SYMBOLS, TRIGGERS } from '@aegisjsproject/component';
+import { html, appendTo } from '@aegisjsproject/core';
+
+class HTMLHelloWorldElement extends AegisComponent {
+	async [SYMBOLS.render](type, { shadow }) {
+		switch(type) {
+			case TRIGGERS.constructed:
+				appendTo(shadow, html`<h1>Hello, World!</h1>`);
+				break;
+		}
+	}
+}
+
+HTMLHelloWorldElement.register('hello-world');
