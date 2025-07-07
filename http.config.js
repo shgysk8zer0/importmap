0c5567966112c910b16f2ed7582d96cf8de6bb1a
Chris Zuber <shgysk8zer0@gmail.com>
2025-07-07 08:42:26 -0700

    Add test
    ### Added
    - Add `npm test` with page + script for testing
    
    ### Changed
    - Get latest updates

--- http.config.js
+++ http.config.js
@@ -1,7 +1,11 @@
-import { getImportmapIntegrity, getImportmapScript } from '@shgysk8zer0/importmap';
+import { getImportmapIntegrity, getImportmapScript, imports } from '@shgysk8zer0/importmap';
 
 const integrity = await getImportmapIntegrity();
 const importmap = await getImportmapScript();
+
+const icon =  `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+	<rect x="0" y="0" width="100" height="100" rx="10" ry="10" fill="#${crypto.getRandomValues(new Uint8Array(3)).toHex()}"></rect>
+</svg>`;
 
 const script = String.dedent`
 	import '@shgysk8zer0/polyfills';
@@ -14,22 +18,37 @@ const script = String.dedent`
 	import { displays, positions, fonts, colors, utilities } from '@aegisjsproject/styles/misc.js';
 	import { transform, transition, animate, keyframes } from '@aegisjsproject/styles/animations.js';
 	import { html } from '@aegisjsproject/core/parsers/html.js';
+	import { css } from '@aegisjsproject/core/parsers/css.js';
 	import { onClick, observeEvents } from '@aegisjsproject/callback-registry/events.js';
+	import { HTMLAegisMDElement } from '@aegisjsproject/aegis-md';
+	import javascript from 'highlight.js/languages/javascript.min.js';
+
+	const custom = css\`[popover] {
+		border: none;
+
+		&::backdrop {
+			background-color: rgba(0, 0, 0, 0.7);
+			backdrop-filter: blur(4px);
+		}
+	\`;
 
 	document.adoptedStyleSheets = [
 		properties, reset, baseTheme, lightTheme, darkTheme, btn, btnPrimary, btnSecondary,
 		btnWarning, btnDanger, scrollbar, displays, positions, fonts, colors, utilities,
-		transform, transition, animate, keyframes,
+		transform, transition, animate, keyframes, custom,
 	];
 
 	trustedTypes.createPolicy('default', {
 		createHTML(input, config) {
+			// Part of the Sanitizer API
 			const doc = Document.parseHTML(input, config);
 			return doc.body.innerHTML;
 		}
 	});
 
-	document.getElementById('main').append(html\`
+	HTMLAegisMDElement.registerLanguages({ javascript });
+
+	document.getElementById('main').prepend(html\`
 		<button
 			type="button"
 			class="btn btn-primary animation-fly-in animation-ease-out animation-speed-normal"
@@ -41,51 +60,70 @@ const script = String.dedent`
 	observeEvents();
 `;
 
+const iconIntegrity = 'sha384-' + await Promise.resolve(new TextEncoder().encode(icon))
+	.then(encoded => crypto.subtle.digest('SHA-384', encoded))
+	.then(hash => new Uint8Array(hash).toBase64());
+
 const scriptIntegrity = 'sha384-' + await Promise.resolve(new TextEncoder().encode(script))
 	.then(encoded => crypto.subtle.digest('SHA-384', encoded))
 	.then(hash => new Uint8Array(hash).toBase64());
+
+const doc = String.dedent`
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8" />
+			<meta name="viewport" content="width=device-width" />
+			<meta name="color-scheme" content="light dark" />
+			<title>@shgysk8zer0/importmap test</title>
+			<link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" referrerpolicy="no-referrer" integrity="${iconIntegrity}" />
+			${importmap}
+			<script integrity="${scriptIntegrity}" type="module">${script}</script>
+		</head>
+		<body>
+			<header id="header"></header>
+			<nav id="nav">
+				<button type="button" class="btn btn-secondary animation-fly-in animation-ease-in animation-speed-fast" popovertarget="md-test">Show Markdown</button>
+			</nav>
+			<main id="main"></main>
+			<aside id="sidebar"></aside>
+			<footer id="footer"></footer>
+			<aegis-md id="md-test" popover="auto">
+				<div slot="markdown">
+					## Hello, World!
+
+					\`\`\`js
+					alert(1);
+					\`\`\`
+				</div>
+			</aegis-md>
+		</body>
+	</html>
+`;
+
+const headers = new Headers({
+	'Content-Type': 'text/html',
+	'Referrer-Policy': 'no-referrer',
+	'Content-Security-Policy': `default-src 'self';
+		script-src 'self' ${Object.values(imports).join(' ')} '${integrity}' '${scriptIntegrity}';
+		style-src 'self' https://unpkg.com/ blob:;
+		img-src 'self' blob: '${iconIntegrity}';
+		trusted-types default aegis-sanitizer#html;
+		require-trusted-types-for 'script';`.replaceAll(/\n\t/g, ''),
+});
 
 export default {
 	port: 8013,
 	pathname: '/test/',
 	open: true,
 	routes: {
+		'/favicon.svg': req => req.destination === 'image'
+			? new Response(icon, { headers: { 'Content-Type': 'image/svg+xml' }})
+			: Response.error(),
 		'/test/': async req => {
-			if (req.destination === 'document') {
-				const doc = String.dedent`
-					<!DOCTYPE html>
-					<html>
-						<head>
-							<meta charset="utf-8" />
-							<meta name="viewport" content="width=device-width" />
-							<meta name="color-scheme" content="light dark" />
-							<title>@shgysk8zer0/importmap test</title>
-							${importmap}
-							<script integrity="${scriptIntegrity}" type="module">${script}</script>
-						</head>
-						<body>
-							<header id="header"></header>
-							<nav id="nav"></nav>
-							<main id="main"></main>
-							<aside id="sidebar"></aside>
-							<footer id="footer"></footer>
-						</body>
-					</html>
-				`;
-
-				const headers = new Headers({
-					'Content-Type': 'text/html',
-					'Referrer-Policy': 'no-referrer',
-					'Content-Security-Policy': `default-src 'self';
-						script-src 'self' '${integrity}' '${scriptIntegrity}' https://unpkg.com/;
-						style-src 'self' https://unpkg.com/ blob:;
-						img-src 'self' blob:;
-						trusted-types default aegis-sanitizer#html;
-						require-trusted-types-for 'script';`.replaceAll(/\n\t/g, ''),
-				});
-
-				return new Response(doc, { headers });
-			}
+			return req.destination === 'document'
+				? new Response(doc, { headers })
+				: Response.error();
 		}
 	}
 };
